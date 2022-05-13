6a612f4a518bbf29f37c55ffb7dd8c8d7b09f746
dhkim4-seegene <dhkim4@seegene.com>
2022-05-13 13:27:57 +0900

    setting up for 21 and 206

---
+++ src/recursion/n_206_reverse_linked_list/Solution.java
@@ -0,0 +1,4 @@
+package recursion.n_206_reverse_linked_list;
+
+public class Solution {
+}
---
+++ src/recursion/n_21_merge_two_sorted_lists/Solution.java
@@ -0,0 +1,15 @@
+package recursion.n_21_merge_two_sorted_lists;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return null;
+    }
+}
