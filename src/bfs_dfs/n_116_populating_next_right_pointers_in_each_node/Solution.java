5221c6459df230183a26162dc97ad6d0e85c85f5
DongHun Kim <donghunkim@DongHunui-Macmini.local>
2022-05-15 20:26:49 +0900

    soled n_116

--- src/bfs_dfs/n_116_populating_next_right_pointers_in_each_node/Solution.java
+++ src/bfs_dfs/n_116_populating_next_right_pointers_in_each_node/Solution.java
@@ -1,4 +1,41 @@
 package bfs_dfs.n_116_populating_next_right_pointers_in_each_node;
 
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+
+
 public class Solution {
+    public Node connect(Node root) {
+        if(root == null){
+            return root;
+        }
+        if(root.left != null){
+            root.left.next = root.right;
+            if(root.next != null){
+                root.right.next = root.next.left;
+            }
+        }
+
+        connect(root.left);
+        connect(root.right);
+        return root;
+    }
 }
