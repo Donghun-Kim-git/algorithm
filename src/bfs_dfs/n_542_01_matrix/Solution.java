ca901c7129a93dbe047b3b32748b64408bb0ac00
donghunkim <ddong3525@gmail.com>
2022-05-17 21:59:41 +0900

    solved n 542

--- src/bfs_dfs/n_542_01_matrix/Solution.java
+++ src/bfs_dfs/n_542_01_matrix/Solution.java
@@ -1,8 +1,29 @@
 package bfs_dfs.n_542_01_matrix;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Solution {
     public int[][] updateMatrix(int[][] mat) {
-
+        Queue<int[]> q = new ArrayDeque<>();
+        int m = mat.length, n = mat[0].length;
+        for(int i=0; i < m; i ++){
+            for(int j = 0; j < n; j ++){
+                if(mat[i][j] == 0) q.offer(new int[]{i, j});
+                else mat[i][j] = -1;
+            }
+        }
+        int[] dir = {-1, 0, 1, 0, -1};
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int r = cur[0], c = cur[1];
+            for(int i = 0; i < 4; i++){
+                int nr = r + dir[i], nc = c + dir[i+1];
+                if(nr < 0 || nc < 0 || nr >= m || nc >= n || mat[nr][nc] != -1) continue;
+                mat[nr][nc] = mat[r][c] + 1;
+                q.offer(new int[]{nr, nc});
+            }
+        }
         return mat;
     }
     public static void main(String[] args){
