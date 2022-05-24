7cab8d4665cf98d52a2d4989de285b73b3a8a065
donghunkim <ddong3525@gmail.com>
2022-05-24 17:22:19 +0900

    solved 88

--- src/data_structure/array/n_88_merge_sorted_array/Solution.java
+++ src/data_structure/array/n_88_merge_sorted_array/Solution.java
@@ -2,8 +2,26 @@ package data_structure.array.n_88_merge_sorted_array;
 
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int shorter = Math.min(m, n);
-        int i = 0, j = 0;
+        int i = m + n - 1;
+        while(i >= 0){
+            if(m > 0 && n > 0) {
+                if (nums1[m - 1] > nums2[n - 1]) {
+                    nums1[i--] = nums1[m - 1];
+                    m--;
+                } else {
+                    nums1[i--] = nums2[n - 1];
+                    n--;
+                }
+            }
+            else if(m > 0){
+                nums1[i--] = nums1[m-1];
+                m--;
+            }
+            else{
+                nums1[i--] = nums2[n-1];
+                n--;
+            }
+        }
 
 
     }
